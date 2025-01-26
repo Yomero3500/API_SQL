a3678099551ceb04ccbd499a0a5d3590a4cc56f0
Yomero3500 <110347908+Yomero3500@users.noreply.github.com>
2025-01-26 11:29:09 -0600

    Manejo mjorado para las imagenes de los productos

--- src/routers/product.router.js
+++ src/routers/product.router.js
@@ -1,42 +1,51 @@
 const router = require('express').Router();
 const Product = require('../models/Product.model');
+const multer = require("multer");
+
 
 router.get("/", async (req, res) => {
-  try { 
-    const product = await Product.findAll();
-    res.send(product);
-  } catch (error) {
-    res.status(500).json({ error: "Ha ocurrido un error" });
-  }
-});
-
-router.get("/:nombre", async (req, res) => {
-  try {
-    const product = await Product.findOne({ where: { nombre: req.params.nombre } });
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404).json({ error: "Producto no encontrado" });
-    }
-  } catch (error) {
-    res.status(500).json({ error: "Ha ocurrido un error" });
-  }
-});
-
-router.post("/", async (req, res) => {
-    const {nombre, precio, imagen } = req.body;
-    try { 
-        await Product.sync()
-        const producto = await Product.create({
-        nombre: nombre,
-        precio: precio,
-        imagen: imagen
-        });
-        res.json(producto);
+    try {
+      const product = await Product.findAll();
+      res.send(product);
     } catch (error) {
-        res.status(500).json({ error: "Ha ocurrido un error" });
-        console.log(error);
+      res.status(500).json({ error: "Ha ocurrido un error" });
     }
+  });
+  
+  router.get("/:nombre", async (req, res) => {
+    try {
+      const product = await Product.findOne({ where: { nombre: req.params.nombre } });
+      if (product) {
+        res.json(product);
+      } else {
+        res.status(404).json({ error: "Producto no encontrado" });
+      }
+    } catch (error) {
+      res.status(500).json({ error: "Ha ocurrido un error" });
+    }
+  });
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+router.post("/", upload.single("image"), async (req, res) => {
+  console.log(req.body);
+  console.log(req.file);
+  const { nombre, precio } = req.body;
+  const imagenBuffer = req.file ? req.file.buffer : null;
+
+  try {
+    await Product.sync();
+    const producto = await Product.create({
+      nombre: nombre,
+      precio: precio,
+      imagen: imagenBuffer,
+    });
+    res.json(producto);
+  } catch (error) {
+    res.status(500).json({ error: "Ha ocurrido un error" });
+    console.log(error);
+  }
 });
 
 router.put("/", async (req, res) => {
